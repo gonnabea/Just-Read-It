b1b257d8e66c35b442a1da5823d2c09852fb7fff
Choi Jiwon <winning1993@naver.com>
2020-02-18 07:45:35 +0900

    Create bookDetail page

--- react-front/Book-detail.jsx
+++ react-front/Book-detail.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 
-const book = this.props.book;
 
 class bookDetail extends React.Component {
     render(){
+        const book = this.props.book
+        return(
         <React.Fragment>
-        <img src={book.imageUrl}/>
+        <img src={`/${book.imageUrl}`}/>
         <h1>{book.title}</h1>
         <h2>{book.totalRate}</h2>
         <h2>{book.author}</h2>
         <h3>{book.description}</h3>
         </React.Fragment>
+        )
     }
 }
 
--- react-front/Home.jsx
+++ react-front/Home.jsx
@@ -10,12 +10,13 @@ function Home(props){
             return (
                 <>
                 {console.log(book)}
+                <a href={props.routes.bookDetail(book.id)}>
                 <img src={book.imageUrl} alt={book.imageUrl}/>
                 <h1>{book.title}</h1>
                 <h2>{book.author}</h2>
                 <h3>{JSON.stringify(book.createdAt)}</h3>
                 <h3>{book.description}</h3>
-                
+                </a>
                 </>
                 )
         })
