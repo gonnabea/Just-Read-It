03fc35ba1d4b88e573218382bda10ab730ae8be7
Choi Jiwon <winning1993@naver.com>
2020-02-24 06:38:01 +0900

    create logo example

--- react-front/title.jsx
+++ react-front/title.jsx
@@ -4,13 +4,13 @@ export default function title(props){
     
     const style={
         display:"flex",
-        width:"50vh",
-        height:"15vh",
+        width:"20vh",
+        height:"20vh",
         margin:"0.7rem",
       }
 
 return     (<a href="/">
-    <img src="images\Book-Worm.jpg" alt="" style={style} />
+    <img src="images\둥근책상-1.jpg" alt="" style={style} />
     </a>);
 
 }
